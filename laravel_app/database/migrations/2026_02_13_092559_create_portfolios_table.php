4bc425dbe54487dc27411c5221982f5f0333da75
izumism8774 <haru.izumism.biz@gmail.com>
2026-02-13 19:00:05 +0900

    マイグレートした

--- laravel_app/database/migrations/2026_02_13_092559_create_portfolios_table.php
+++ laravel_app/database/migrations/2026_02_13_092559_create_portfolios_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -11,6 +12,8 @@ return new class extends Migration
      */
     public function up(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        
         Schema::create('portfolios', function (Blueprint $table) {
             $table->id();
             $table->string('title');
@@ -23,6 +26,8 @@ return new class extends Migration
             $table->foreignId('stacks_id')->constrained('stacks')->onDelete('cascade');
             $table->timestamps();
         });
+        
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 
     /**
@@ -30,6 +35,8 @@ return new class extends Migration
      */
     public function down(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Schema::dropIfExists('portfolios');
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 };
